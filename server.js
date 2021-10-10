cf391e48ac1c845dd59e469e55cec67ac70475b9
prinako <nyankonton122@gmail.com>
2021-10-10 02:18:13 -0300

    fix problem with cors

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
 io.on("connection", (socket) => {
   const id = socket.handshake.query.id;
+  console.log(id);
   socket.join(id);
   socket.on("send-message", ({ recipients, text }) => {
     recipients.forEach((recipient) => {
